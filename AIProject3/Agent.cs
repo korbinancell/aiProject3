6f71f30b67eb2b7c75285e21e68eec284239b856
vxp12c <vxp12c@acu.edu>
2017-04-19 01:02:10 -0500

    progress :) ?

--- AIProject3/Agent.cs
+++ AIProject3/Agent.cs
@@ -8,6 +8,8 @@ namespace AIProject3
 {
     class Agent
     {
+        double decayFactor;
+        bool favorExploration;
         char token;
         string filename;
         Agent opponent;
@@ -17,8 +19,10 @@ namespace AIProject3
         Stack<Tuple<StringBuilder, int>> previousMoves;
         Dictionary<StringBuilder, double[]> statetionary = new Dictionary<StringBuilder, double[]>();
 
-        public Agent(string filenm, char tokn, StringBuilder board, Agent opnt = null)
+        public Agent(string filenm, char tokn, StringBuilder board, Agent opnt = null, bool favrExplrtn = true)
         {
+            decayFactor = .5;
+            
             filename = filenm;
             token = tokn;
             currentBoard = board;
@@ -28,8 +32,15 @@ namespace AIProject3
             rando = new Random(Guid.NewGuid().GetHashCode());
             previousMoves = new Stack<Tuple<StringBuilder, int>>();
             statetionary = new Dictionary<StringBuilder, double[]>();
+
+            Console.WriteLine(currentBoard);
         }
 
+        public StringBuilder getCurrentBoard()
+        {
+            return currentBoard;
+        }
+        
         private bool seenState()
         {
             StringBuilder test, cb = currentBoard;
@@ -123,25 +134,76 @@ namespace AIProject3
 
         }
 
-        public void makeMove()
+        public void makeMove(StringBuilder boardRigtNow)
         {
-            if(seenState())
+            Console.WriteLine("makeMove function");
+            currentBoard = boardRigtNow;
+            if(!seenState())
             {
-                if (playingAgent)
-                {
-                    Tuple<int, double>[] sorted = new Tuple<int, double>[9];
-                    for(int i=0;i<9;i++)
-                        sorted[i] = Tuple.Create(i, statetionary[currentBoard][i]);
+                double[] h = { 0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5};
+                statetionary.Add(currentBoard, h);
+            }
+            //if (playingAgent)
+            //{
+                Tuple<int, double>[] sorted = new Tuple<int, double>[9];
+                for(int i=0;i<9;i++)
+                    sorted[i] = Tuple.Create(i, statetionary[currentBoard][i]);
 
-                    Array.Sort(sorted, (x,y) =>  x.Item2.CompareTo(y.Item2));
+                Array.Sort(sorted, (x,y) =>  x.Item2.CompareTo(y.Item2));
 
-                    int a = Math.Max(rando.Next(9), rando.Next(9));
+                int a = Math.Max(rando.Next(9), rando.Next(9));
+                while(currentBoard[a] != '_')
+                    a = Math.Max(rando.Next(9), rando.Next(9));
 
-                    previousMoves.Push(Tuple.Create(new StringBuilder(currentBoard.ToString()), sorted[a].Item1));
+                previousMoves.Push(Tuple.Create(new StringBuilder(currentBoard.ToString()), sorted[a].Item1));
+                currentBoard[ sorted[a].Item1 ] = token;
+            //}
+        }
 
-                    currentBoard[ sorted[a].Item1 ] = token;
-                }
+        public bool hasWon(char tkn)
+        {
+            StringBuilder tempo = new StringBuilder();
+            tempo = currentBoard;    
+
+            for(int i = 0; i < 4; i++)
+            {
+                //straight on the side
+                if (tempo[0] == tkn && tempo[1] == tkn && tempo[2] == tkn)
+                    return true;
+                //straight throught the middle
+                if (tempo[3] == tkn && tempo[4] == tkn && tempo[5] == tkn)
+                    return true;
+                //diagonally through the middle
+                if (tempo[0] == tkn && tempo[4] == tkn && tempo[8] == tkn)
+                    return true;
+                var test = new StringBuilder(tempo[2].ToString() + tempo[1].ToString() + tempo[0].ToString() + tempo[5].ToString() + tempo[4].ToString() + tempo[3].ToString() + tempo[8].ToString() + tempo[7].ToString() + tempo[6].ToString());
+                tempo = test;
+            }
+
+            return false;
+        }
+
+        public void giveReinforcement(bool didIWin)
+        {
+            var mostRecentmove = previousMoves.Pop();
+
+            Console.WriteLine(statetionary.ContainsKey(mostRecentmove.Item1));
+// Ok, the problem is this guy right here v I think we're adding states to the statetionary at like half a step off, like for the opponent instead of ourselves, so we have all of the other states we need, but not actually the ones on the stack, so we're looking for something that isn't there and that gives us the error. We need to go fix that. Make it so.
+            double[] values = statetionary[mostRecentmove.Item1];
+            double currentDecay = decayFactor;
+
+            values[mostRecentmove.Item2] = didIWin ? 1 : values[mostRecentmove.Item2] - (values[mostRecentmove.Item2] * currentDecay);
+            statetionary.Add(mostRecentmove.Item1, values);
+            currentDecay /= 2;
+
+            while(previousMoves.Count > 0)
+            {
+                mostRecentmove = previousMoves.Pop();
+                values = statetionary[mostRecentmove.Item1];
+                values[mostRecentmove.Item2] = didIWin ? values[mostRecentmove.Item2] + (values[mostRecentmove.Item2] * currentDecay) : values[mostRecentmove.Item2] - (values[mostRecentmove.Item2] * currentDecay);
+                currentDecay /= 2;
             }
         }
+
     }
 }
--- AIProject3/Program.cs
+++ AIProject3/Program.cs
@@ -10,8 +10,77 @@ namespace AIProject3
     {
         static void Main(string[] args)
         {
-            StringBuilder test;
-            StringBuilder cb = new StringBuilder("OXX______");
+            //play itself
+            Agent x = new Agent("temp_filename", 'X', new StringBuilder("_________"));
+            Agent o = new Agent("temp_filename", 'O', new StringBuilder("_________"), x);
+            int ctr = 0;
+
+            while (true)
+            {
+                var test = x.getCurrentBoard();
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int q = 0; q < 3; q++)
+                    {
+                        Console.Write(test[i * 3 + q]);
+                    }
+                    Console.WriteLine();
+                }
+
+                if (x.hasWon('X'))
+                {
+                    x.giveReinforcement(true);
+                    o.giveReinforcement(false);
+                    Console.WriteLine("X wins");
+                    break;
+                }
+                else if (x.hasWon('O'))
+                {
+                    x.giveReinforcement(false);
+                    o.giveReinforcement(true);
+                    Console.WriteLine("O wins");
+                    break;
+                }
+                else if (!x.getCurrentBoard().ToString().Contains('_'))
+                {
+                    var cb = x.getCurrentBoard().ToString();
+                    break;
+                }
+                if(ctr%2 == 0)
+                {
+                    x.makeMove(o.getCurrentBoard());
+                }
+                else 
+                {
+                    o.makeMove(x.getCurrentBoard());
+                }
+                ctr++;
+              }
+
+            /*StringBuilder test;
+            StringBuilder cb = new StringBuilder("XOOOXOOOX");
+
+            StringBuilder tempo = new StringBuilder();
+            tempo = cb;
+            char tkn = 'X';
+
+            for (int i = 0; i < 4; i++)
+            {
+                //straight on the side
+                Console.WriteLine(tempo[0] == tkn && tempo[1] == tkn && tempo[2] == tkn);
+                //Console.WriteLine(true);
+                //straight throught the middle
+                Console.WriteLine(tempo[3] == tkn && tempo[4] == tkn && tempo[5] == tkn);
+                //Console.WriteLine(true);
+                //diagonally through the middle
+                Console.WriteLine(tempo[0] == tkn && tempo[4] == tkn && tempo[8] == tkn);
+                    //Console.WriteLine(true);
+                var thing = new StringBuilder(tempo[6].ToString() + tempo[3].ToString() + tempo[0].ToString() + tempo[7].ToString() + tempo[4].ToString() + tempo[1].ToString() + tempo[8].ToString() + tempo[5].ToString() + tempo[2].ToString());
+                tempo = thing;
+            }
+            */
+
+            /*
             Console.WriteLine("Normal");
             for (int i = 0; i < 3; i++)
             {
@@ -102,6 +171,7 @@ namespace AIProject3
                 Console.WriteLine();
             }
             Console.WriteLine("\n");
+            */
         }
     }
 }
