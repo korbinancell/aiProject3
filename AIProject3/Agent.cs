e9f8814f6e16018f0df6e43fe6ed62071cf0a197
vxp12c <vxp12c@acu.edu>
2017-04-20 00:25:42 -0500

    It works!!! :D

--- AIProject3/Agent.cs
+++ AIProject3/Agent.cs
@@ -19,7 +19,7 @@ namespace AIProject3
         Stack<Tuple<StringBuilder, int>> previousMoves;
         Dictionary<string, double[]> statetionary = new Dictionary<string, double[]>();
 
-        public Agent(string filenm, char tokn, StringBuilder board, Agent opnt = null, bool favrExplrtn = true)
+        public Agent(string filenm, char tokn, StringBuilder board, bool favrExplrtn = true)
         {
             decayFactor = .5;
             favorExploration = favrExplrtn;
@@ -27,14 +27,12 @@ namespace AIProject3
             filename = filenm;
             token = tokn;
             currentBoard = board;
-            opponent = opnt;
             playingAgent = !(opponent == null);
 
             rando = new Random(Guid.NewGuid().GetHashCode());
             previousMoves = new Stack<Tuple<StringBuilder, int>>();
             statetionary = new Dictionary<string, double[]>();
 
-            Console.WriteLine(currentBoard);
         }
 
         public StringBuilder getCurrentBoard()
@@ -42,9 +40,19 @@ namespace AIProject3
             return currentBoard;
         }
 
+        public void setcurrentBoard(StringBuilder cb)
+        {
+            currentBoard = cb;
+        }
+
         public Dictionary<string, double[]> getStatetionary()
         {
             return statetionary;
+        }
+
+        public void setStatetionary (Dictionary<string, double[]> ssnry)
+        {
+            statetionary = ssnry;
         }
 
         private bool seenState()
@@ -58,117 +66,133 @@ namespace AIProject3
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[2] + hd[1] + hd[0] + hd[5] + hd[4] + hd[3] + hd[8] + hd[7] + hd[6] };
-
+                turned = new double[] { hd[2] , hd[1] , hd[0] , hd[5] , hd[4] , hd[3] , hd[8] , hd[7] , hd[6] };
+                
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //vFlip
             test = new StringBuilder(cb[6].ToString() + cb[7].ToString() + cb[8].ToString() + cb[3].ToString() + cb[4].ToString() + cb[5].ToString() + cb[0].ToString() + cb[1].ToString() + cb[2].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[6] + hd[7] + hd[8] + hd[3] + hd[4] + hd[5] + hd[0] + hd[1] + hd[2] };
+                turned = new double[] { hd[6] , hd[7] , hd[8] , hd[3] , hd[4] , hd[5] , hd[0] , hd[1] , hd[2] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //r90
             test = new StringBuilder(cb[6].ToString() + cb[3].ToString() + cb[0].ToString() + cb[7].ToString() + cb[4].ToString() + cb[1].ToString() + cb[8].ToString() + cb[5].ToString() + cb[2].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[2] + hd[5] + hd[8] + hd[1] + hd[4] + hd[7] + hd[0] + hd[3] + hd[6] };
+                turned = new double[] { hd[2] , hd[5] , hd[8] , hd[1] , hd[4] , hd[7] , hd[0] , hd[3] , hd[6] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //r90vFlip
             test = new StringBuilder(cb[8].ToString() + cb[5].ToString() + cb[2].ToString() + cb[7].ToString() + cb[4].ToString() + cb[1].ToString() + cb[6].ToString() + cb[3].ToString() + cb[0].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[8] + hd[5] + hd[2] + hd[7] + hd[4] + hd[1] + hd[6] + hd[3] + hd[0] };
+                turned = new double[] { hd[8] , hd[5] , hd[2] , hd[7] , hd[4] , hd[1] , hd[6] , hd[3] , hd[0] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //r180
             test = new StringBuilder(cb[8].ToString() + cb[7].ToString() + cb[6].ToString() + cb[5].ToString() + cb[4].ToString() + cb[3].ToString() + cb[2].ToString() + cb[1].ToString() + cb[0].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[8] + hd[7] + hd[6] + hd[5] + hd[4] + hd[3] + hd[2] + hd[1] + hd[0] };
+                turned = new double[] { hd[8] , hd[7] , hd[6] , hd[5] , hd[4] , hd[3] , hd[2] , hd[1] , hd[0] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //r270
             test = new StringBuilder(cb[2].ToString() + cb[5].ToString() + cb[8].ToString() + cb[1].ToString() + cb[4].ToString() + cb[7].ToString() + cb[0].ToString() + cb[3].ToString() + cb[6].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[6] + hd[3] + hd[0] + hd[7] + hd[4] + hd[1] + hd[8] + hd[5] + hd[2] };
+                turned = new double[] { hd[6] , hd[3] , hd[0] , hd[7] , hd[4] , hd[1] , hd[8] , hd[5] , hd[2] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             //r270vFlup
             test = new StringBuilder(cb[0].ToString() + cb[3].ToString() + cb[6].ToString() + cb[1].ToString() + cb[4].ToString() + cb[7].ToString() + cb[2].ToString() + cb[5].ToString() + cb[8].ToString()).ToString();
             if (statetionary.ContainsKey(test))
             {
                 hd = statetionary[test];
-                turned = new double[] { hd[0] + hd[3] + hd[6] + hd[1] + hd[4] + hd[7] + hd[2] + hd[5] + hd[8] };
+                turned = new double[] { hd[0] , hd[3] , hd[6] , hd[1] , hd[4] , hd[7] , hd[2] , hd[5] , hd[8] };
 
                 statetionary.Remove(test);
                 statetionary.Add(currentBoard.ToString(), turned);
                 return true;
             }
-
+            
             return statetionary.ContainsKey(currentBoard.ToString());
 
         }
 
         public void makeMove(StringBuilder boardRigtNow)
         {
-            //Console.WriteLine("makeMove function");
             currentBoard = boardRigtNow;
-            if(!seenState())
+            
+            if (!seenState())
             {
                 double[] h = { 0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5};
                 statetionary.Add(currentBoard.ToString(), h);
-                //Console.WriteLine("hello from makeMove");
             }
+            
             if (favorExploration)
             {
                 Tuple<int, double>[] sorted = new Tuple<int, double>[9];
-                for(int i=0;i<9;i++)
+                
+                for (int i=0;i<9;i++)
                     sorted[i] = Tuple.Create(i, statetionary[currentBoard.ToString()][i]);
 
                 Array.Sort(sorted, (x,y) =>  x.Item2.CompareTo(y.Item2));
 
                 int a = Math.Max(rando.Next(9), rando.Next(9));
-                while(currentBoard[a] != '_')
+                while(currentBoard[sorted[a].Item1] != '_')
                     a = Math.Max(rando.Next(9), rando.Next(9));
 
-
                 previousMoves.Push(Tuple.Create(new StringBuilder(currentBoard.ToString()), sorted[a].Item1));
-                //Console.WriteLine("current board is {0}", currentBoard.ToString());
                 currentBoard[ sorted[a].Item1 ] = token; 
-                //Console.WriteLine("current board is {0}", currentBoard.ToString());
+            }
+            else
+            {
+                Tuple<int, double>[] sorted = new Tuple<int, double>[9];
 
+                for (int i = 0; i < 9; i++)
+                    sorted[i] = Tuple.Create(i, statetionary[currentBoard.ToString()][i]);
+
+                Array.Sort(sorted, (x, y) => x.Item2.CompareTo(y.Item2));
+
+                for (int i = 8; i >= 0; i--)
+                    Console.WriteLine(sorted[i]);
+
+                int ctr = 8;
+                while(currentBoard[sorted[ctr].Item1] != '_')
+                    ctr--;
+
+                previousMoves.Push(Tuple.Create(new StringBuilder(currentBoard.ToString()), sorted[ctr].Item1));
+                currentBoard[sorted[ctr].Item1] = token;
             }
         }
 
--- AIProject3/Program.cs
+++ AIProject3/Program.cs
@@ -10,192 +10,200 @@ namespace AIProject3
     {
         static void Main(string[] args)
         {
-            //play itself
-            Agent x = new Agent("temp_filename", 'X', new StringBuilder("_________"));
-            Agent o = new Agent("temp_filename", 'O', new StringBuilder("_________"));
-            int ctr = 0;
+            Console.WriteLine("Is a person playing? Y/N");
+            string input = Console.ReadLine();
 
-            while (true)
+            if (input == "Y")
             {
-                var test = x.getCurrentBoard();
-                for (int i = 0; i < 3; i++)
+                Agent x = new Agent("temp_filename", 'X', new StringBuilder("_________"), false);
+                DoFile writerA = new DoFile();
+                var Astionary = writerA.ReadStatetionary("test.txt");
+                x.setStatetionary(Astionary);
+                StringBuilder humanBoard = x.getCurrentBoard();
+
+                Console.WriteLine("Do you wanna go first? Y/N");
+                bool first = Console.ReadLine() == "Y";
+
+                if(!first)
+                    x.makeMove(x.getCurrentBoard());
+
+                while (true)
                 {
-                    for (int q = 0; q < 3; q++)
+                    humanBoard = x.getCurrentBoard();
+                    x.setcurrentBoard(humanBoard);
+
+                    var test = x.getCurrentBoard();
+                    for (int i = 0; i < 3; i++)
                     {
-                        Console.Write(test[i * 3 + q]);
+                        for (int q = 0; q < 3; q++)
+                        {
+                            Console.Write(test[i * 3 + q]);
+                        }
+                        Console.WriteLine();
                     }
                     Console.WriteLine();
-                }
-                Console.WriteLine();
 
-                if (x.hasWon('X'))
-                {
-                    Console.WriteLine("X wins");
-                    x.giveReinforcement(true);
-                    o.giveReinforcement(false);
-                    /*foreach (var k in x.getStatetionary())
+                    Console.WriteLine("Which space do you wanna go? 0...8");
+                    int place;
+                    Int32.TryParse(Console.ReadLine(), out place);
+
+                    humanBoard[place] = 'O';
+                    x.setcurrentBoard(humanBoard);
+
+                    var Atest = humanBoard;
+                    for (int i = 0; i < 3; i++)
                     {
-                        Console.WriteLine("key: {0} value: ", k.Key);
-                        foreach (var d in k.Value)
+                        for (int q = 0; q < 3; q++)
                         {
-                            Console.Write("{0} ", d);
+                            Console.Write(Atest[i * 3 + q]);
                         }
-                    }*/
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
 
-                    break;
-                }
-                else if (x.hasWon('O'))
-                {
-                    Console.WriteLine("O wins");
-                    x.giveReinforcement(false);
-                    o.giveReinforcement(true);
-                    /*foreach (var k in x.getStatetionary())
+                    if (x.hasWon('X'))
                     {
-                        Console.WriteLine("key: {0} value: ", k.Key);
-                        foreach (var d in k.Value)
+                        Console.WriteLine("You Lost! :(");
+                        x.giveReinforcement(true);
+                        break;
+                    }
+                    else if (x.hasWon('O'))
+                    {
+                        Console.WriteLine("You Win! :)");
+                        x.giveReinforcement(false);
+                        break;
+                    }
+                    else if (!x.getCurrentBoard().ToString().Contains('_'))
+                    {
+                        Console.WriteLine("Tie Game");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("X gets to move now: ");
+                        x.makeMove(humanBoard);
+                        humanBoard = x.getCurrentBoard();
+                    }
+                    
+                    if (x.hasWon('X'))
+                    {
+                        x.giveReinforcement(true);
+                        var Btest = x.getCurrentBoard();
+                        for (int i = 0; i < 3; i++)
                         {
-                            Console.Write("{0} ", d);
+                            for (int q = 0; q < 3; q++)
+                            {
+                                Console.Write(Btest[i * 3 + q]);
+                            }
+                            Console.WriteLine();
                         }
-                    }*/
+                        Console.WriteLine();
+                        Console.WriteLine("You Lost! :(");
+                        break;
+                    }
+                    else if (x.hasWon('O'))
+                    {
+                        Console.WriteLine("You Win! :)");
+                        x.giveReinforcement(false);
+                        break;
+                    }
+                    else if (!x.getCurrentBoard().ToString().Contains('_'))
+                    {
+                        Console.WriteLine("Tie Game");
+                        break;
+                    }
+
+                    DoFile Bwriter = new DoFile();
+                    Bwriter.WriteStatetionary("test.txt", x.getStatetionary());
                 }
-                else if (!x.getCurrentBoard().ToString().Contains('_'))
+            }
+            else
+            {
+                for (int loop = 0; loop < 1000; loop++)
                 {
-                    Console.WriteLine("Tie Game");
-                    break;
+                    if (loop % 10 == 0)
+                        Console.WriteLine(loop);
+                    //play itself
+                    Agent x = new Agent("temp_filename", 'X', new StringBuilder("_________"));
+                    Agent o = new Agent("temp_filename", 'O', new StringBuilder("_________"));
+                    int ctr = 0;
+
+                    DoFile writerA = new DoFile();
+
+                    //                Console.WriteLine("Read:");
+                    var Astionary = writerA.ReadStatetionary("test.txt");
+                    //foreach (var blep in Astionary)
+                    //    Console.WriteLine("{0}: {1}", blep.Key, String.Join(",", blep.Value.Select(p => p.ToString()).ToArray()));
+
+                    x.setStatetionary(Astionary);
+                    o.setStatetionary(Astionary);
+
+                    while (true)
+                    {
+                        var test = x.getCurrentBoard();
+                       /*                        for (int i = 0; i < 3; i++)
+                                                {
+                                                    for (int q = 0; q < 3; q++)
+                                                    {
+                                                        Console.Write(test[i * 3 + q]);
+                                                    }
+                                                    Console.WriteLine();
+                                                }
+                                                Console.WriteLine();
+                        */
+                        if (x.hasWon('X'))
+                        {
+//                            Console.WriteLine("X wins");
+                            x.giveReinforcement(true);
+                            o.giveReinforcement(false);
+                            /*foreach (var k in x.getStatetionary())
+                            {
+                                Console.WriteLine("key: {0} value: ", k.Key);
+                                foreach (var d in k.Value)
+                                {
+                                    Console.Write("{0} ", d);
+                                }
+                            }*/
+
+                            break;
+                        }
+                        else if (x.hasWon('O'))
+                        {
+ //                           Console.WriteLine("O wins");
+                            x.giveReinforcement(false);
+                            o.giveReinforcement(true);
+                            /*foreach (var k in x.getStatetionary())
+                            {
+                                Console.WriteLine("key: {0} value: ", k.Key);
+                                foreach (var d in k.Value)
+                                {
+                                    Console.Write("{0} ", d);
+                                }
+                            }*/
+                            break;
+                        }
+                        else if (!x.getCurrentBoard().ToString().Contains('_'))
+                        {
+ //                           Console.WriteLine("Tie Game");
+                            break;
+                        }
+                        if (ctr % 2 == 0)
+                            x.makeMove(o.getCurrentBoard());
+                        else
+                            o.makeMove(x.getCurrentBoard());
+                        ctr++;
+                    }
+
+                    DoFile writer = new DoFile();
+
+                    //                Console.WriteLine("Write:");
+                    writer.WriteStatetionary("test.txt", x.getStatetionary());
+
+
+                    //foreach (var blep in stionary)
+                    //    Console.WriteLine("{0}: {1}", blep.Key, String.Join(",", blep.Value.Select(p => p.ToString()).ToArray()));
                 }
-                if(ctr%2 == 0)
-                    x.makeMove(o.getCurrentBoard());
-                else 
-                    o.makeMove(x.getCurrentBoard());
-                ctr++;
-              }
-
-            DoFile writer = new DoFile();
-
-            Console.WriteLine("Write:");
-            writer.WriteStatetionary("test.txt", x.getStatetionary());
-
-            Console.WriteLine("Read:");
-            var stionary = writer.ReadStatetionary("test.txt");
-
-            foreach (var blep in stionary)
-                Console.WriteLine("{0}: {1}", blep.Key, String.Join(",", blep.Value.Select(p => p.ToString()).ToArray()));
-
-            /*StringBuilder test;
-            StringBuilder cb = new StringBuilder("XOOOXOOOX");
-
-            StringBuilder tempo = new StringBuilder();
-            tempo = cb;
-            char tkn = 'X';
-
-            for (int i = 0; i < 4; i++)
-            {
-                //straight on the side
-                Console.WriteLine(tempo[0] == tkn && tempo[1] == tkn && tempo[2] == tkn);
-                //Console.WriteLine(true);
-                //straight throught the middle
-                Console.WriteLine(tempo[3] == tkn && tempo[4] == tkn && tempo[5] == tkn);
-                //Console.WriteLine(true);
-                //diagonally through the middle
-                Console.WriteLine(tempo[0] == tkn && tempo[4] == tkn && tempo[8] == tkn);
-                    //Console.WriteLine(true);
-                var thing = new StringBuilder(tempo[6].ToString() + tempo[3].ToString() + tempo[0].ToString() + tempo[7].ToString() + tempo[4].ToString() + tempo[1].ToString() + tempo[8].ToString() + tempo[5].ToString() + tempo[2].ToString());
-                tempo = thing;
-            }
-            */
-
-            /*
-            Console.WriteLine("Normal");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                {
-                    Console.Write(cb[i * 3 + q]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //hFlip
-            test = new StringBuilder(cb[2].ToString() + cb[1].ToString() + cb[0].ToString() + cb[5].ToString() + cb[4].ToString() + cb[3].ToString() + cb[8].ToString() + cb[7].ToString() + cb[6].ToString());
-           
-            Console.WriteLine("hFlip");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                {
-                    Console.Write(test[i * 3 + q]);
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //vFlip
-            test = new StringBuilder(cb[6].ToString() + cb[7].ToString() + cb[8].ToString() + cb[3].ToString() + cb[4].ToString() + cb[5].ToString() + cb[0].ToString() + cb[1].ToString() + cb[2].ToString());
-            Console.WriteLine("vFlip");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //r90
-            test = new StringBuilder(cb[6].ToString() + cb[3].ToString() + cb[0].ToString() + cb[7].ToString() + cb[4].ToString() + cb[1].ToString() + cb[8].ToString() + cb[5].ToString() + cb[2].ToString());
-            Console.WriteLine("r90");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //r90vFlip
-            test = new StringBuilder(cb[8].ToString() + cb[5].ToString() + cb[2].ToString() + cb[7].ToString() + cb[4].ToString() + cb[1].ToString() + cb[6].ToString() + cb[3].ToString() + cb[0].ToString());
-            Console.WriteLine("r90vFlip");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //r180
-            test = new StringBuilder(cb[8].ToString() + cb[7].ToString() + cb[6].ToString() + cb[5].ToString() + cb[4].ToString() + cb[3].ToString() + cb[2].ToString() + cb[1].ToString() + cb[0].ToString());
-            Console.WriteLine("r180");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //r270
-            test = new StringBuilder(cb[2].ToString() + cb[5].ToString() + cb[8].ToString() + cb[1].ToString() + cb[4].ToString() + cb[7].ToString() + cb[0].ToString() + cb[3].ToString() + cb[6].ToString());
-            Console.WriteLine("r270");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-
-            //r270vFlup
-            test = new StringBuilder(cb[0].ToString() + cb[3].ToString() + cb[6].ToString() + cb[1].ToString() + cb[4].ToString() + cb[7].ToString() + cb[2].ToString() + cb[5].ToString() + cb[8].ToString());
-            Console.WriteLine("r270vFlup");
-            for (int i = 0; i < 3; i++)
-            {
-                for (int q = 0; q < 3; q++)
-                    Console.Write(test[i * 3 + q]);
-                Console.WriteLine();
-            }
-            Console.WriteLine("\n");
-            */
+            }                       
         }
     }
 }
